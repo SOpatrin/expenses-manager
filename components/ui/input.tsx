b0a309c44f914d83e1d79d10b53db211b26a0db7
Vladislav Potapov <15252vlad@gmail.com>
2026-06-12 22:55:36 +0200

    fix: allow decimal amounts, hide number input spinners

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -7,7 +7,7 @@ export function Input({
   return (
     <input
       className={cn(
-        'rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm outline-none focus:border-zinc-400 dark:border-zinc-700 dark:bg-zinc-900',
+        '[appearance:textfield] rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm outline-none focus:border-zinc-400 dark:border-zinc-700 dark:bg-zinc-900 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
         className,
       )}
       {...props}
